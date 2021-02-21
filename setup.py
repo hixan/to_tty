c64f87dd034fae6326e1adb29fcc8942f4351ace
Hixan <alex.88.elias@gmail.com>
2021-02-21 21:03:05 +1100

    add information for pypi

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
 from setuptools import setup
 setup(
         name='to_tty',
+        author='Hixan',
+        author_email='Alex.88.Elias@gmail.com',
         version='1.0',
         py_modules=['to_tty'],
         install_requires=['Click', 'to_tty'],
